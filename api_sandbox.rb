f8b397f457720e52dccd05e191972c36e938f234
Ryan Case <rmcase@gmail.com>
2014-02-04 17:39:44 -0700

    Changed methods to use the same naming conventions as the WOT API methods

--- api_sandbox.rb
+++ api_sandbox.rb
@@ -4,43 +4,62 @@ require 'json'
 WOT_HOST = 'api.worldoftanks.com'
 APP_ID = '19c9589415e3eefac7daf8039b2e5f1f'
 
-def tank_list
-  request_string = "/wot/encyclopedia/tanks/?application_id=#{APP_ID}"
+def wot_api_request request_string
   Net::HTTP.get(WOT_HOST, request_string)
 end
 
-def tank_json
-  tank_string = tank_list
-  tank_hash = JSON.parse(tank_list)
-  if tank_hash['status'] == 'ok'
-    tank_json = tank_hash['data']
+def hash_from_request_string request_string
+  data = JSON.parse(wot_api_request(request_string))
+  if data['status'] == 'ok'
+    final = data['data']
   else
-    puts "Error retrieving data"
+    raise "Error loading data"
   end
 end
 
-def tank_info tank_id
-  Net::HTTP.get(WOT_HOST, 
-                "/wot/encyclopedia/tankinfo/?application_id=#{APP_ID}&tank_id=#{tank_id}")
+def list_of_vehicles
+  request_string = "/wot/encyclopedia/tanks/?application_id=#{APP_ID}"
+  tanks_hash = hash_from_request_string(request_string)
 end
 
-puts "Total number of tanks: #{tank_json.count}"
-
-# Make an array of all the tank id's
-id_arr = []
-tank_json.each_key { |key| id_arr.push(tank_json[key]['id']) }
-
-# Counting the tanks by nationality 
-nationality_count = Hash.new()
-tank_json.each_key do |key|
-  nationality = tank_json[key]['nation_i18n']
-  if not nationality_count[nationality]
-    nationality_count[nationality] = 1
-  else
-    nationality_count[nationality] += 1
-  end
+def vehicle_details tank_id
+  request_string = 
+    "/wot/encyclopedia/tankinfo/?application_id=#{APP_ID}&tank_id=#{tank_id}"
+  hash_from_request_string(request_string)
 end
 
-nationality_count.each { |key, value| puts "#{key}: #{value}" }
+def module_details module_id, module_type_string
+  request_string = 
+    "/wot/encyclopedia/#{module_type_string}/?application_id=#{APP_ID}&module_id=#{module_id}"
+  hash_from_request_string(request_string)
+end
 
-puts tank_info(49)
+def engine_details module_id
+  module_details(module_id, "tankengines")
+end
+
+def turret_details module_id
+  module_details(module_id, "tankturrets")
+end
+
+def radio_details module_id
+  module_details(module_id, "tankradios")
+end
+
+def suspension_details module_id
+  module_details(module_id, "tankchassis")
+end
+
+def gun_details module_id
+  module_details(module_id, "tankguns")
+end
+
+### Helper Methods 
+
+def all_tanks_id_array
+  id_arr = []
+  tanks_hash = list_of_vehicles
+  tanks_hash.each_key { |key| id_arr.push(tanks_hash[key]['id']) }
+end
+
+puts vehicle_details(49)
