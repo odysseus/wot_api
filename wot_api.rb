34cdb1279dc05382e8a11274e42cc3ccc5eff625
Ryan Case <rmcase@gmail.com>
2014-02-20 14:43:03 -0700

    separated the direct api methods into a new file

--- wot_api.rb
+++ wot_api.rb
@@ -22,6 +22,22 @@ def list_of_vehicles
   tanks_hash = hash_from_request_string(request_string)
 end
 
+def sorted_vehicle_dict
+  # Create the structure for the sorted dictionary
+  sorted_dict = {}
+  (1..10).each { |x| sorted_dict["tier#{x}"] = {} }
+  # Pull the data
+  raw_list = $list_of_vehicles
+  raw_list.each_key do |key|
+    if not sorted_dict["tier#{raw_list[key]['level']}"][raw_list[key]['type']]
+      sorted_dict["tier#{raw_list[key]['level']}"][raw_list[key]['type']] = [raw_list[key]['tank_id']]
+    else
+      sorted_dict["tier#{raw_list[key]['level']}"][raw_list[key]['type']].push(raw_list[key]['tank_id'])
+    end
+  end
+  return sorted_dict
+end
+
 # The first two levels of the hash returned by the API are stripped, retaining
 # only the hash containing the tank data itself. Note that this includes 
 # removing the tank id number
@@ -61,49 +77,3 @@ end
 def gun_details module_id
   module_details(module_id, "tankguns")
 end
-
-# Extract the array of modules for a given tank and module type, valid module
-# types are "chassis", "engines", "guns", "radios", "turrets"
-def list_available_modules tank_id, module_type
-  tank = vehicle_details(tank_id)
-  modules = tank[module_type]
-  details = []
-  modules.each do |item|
-    details.push(module_details(item['module_id'], "tank#{module_type}"))
-  end
-  return details
-end
-
-def list_available_module_names tank_id, module_type
-  modules = list_available_modules(tank_id, module_type)
-  names = []
-  modules.each { |item| names.push(item['name_i18n']) }
-  return names
-end
-
-### Helper Methods 
-
-# Creates an array of all tank ids, useful for iterating through every tank
-def all_tanks_id_array
-  id_arr = []
-  tanks_hash = list_of_vehicles
-  tanks_hash.each_key { |key| id_arr.push(tanks_hash[key]['id']) }
-end
-
-# Creates a dynamic tally of all tanks based on the tank-level attribute supplied
-# as the argument. EG: all_tanks_count_by "type" would tally tanks by their class
-# "nation_i18n" would do it by country. It works for any top level element, though
-# there are only a handful of keys for which this kind of counting would be useful
-def all_tanks_count_by key
-  tanks_hash = list_of_vehicles
-  tally = Hash.new
-  tanks_hash.each_key do |tank|
-    if tally[tanks_hash[tank][key]]
-      tally[tanks_hash[tank][key]] += 1
-    else
-      tally[tanks_hash[tank][key]] = 1
-    end
-  end
-  return tally
-end
-
