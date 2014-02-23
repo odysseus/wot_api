42ec6caffdbeb78641d4dda514d839e4d6aa5463
Ryan Case <rmcase@gmail.com>
2014-02-22 22:35:42 -0700

    Basic script to convert the new data is in place

--- alt_conversion.rb
+++ alt_conversion.rb
@@ -17,6 +17,14 @@ def is_premium? price_kind
   end
 end
 
+def premium_shell? type
+  if type == "gold"
+    return true
+  else
+    return false
+  end
+end
+
 # Alternate data source doesn't include any reliable way to determine whether 
 # the tank has a turret or not, so this reads the converted API JSON and makes
 # a simple hash of all tanks with the name as the key and true/false as the 
@@ -83,22 +91,22 @@ def tank_hash tank
   has_turret = $turret_hash[tank['userstring']]
   t['name'] = tank['userstring']
   t['nationality'] = nation
-  t['tier'] = tank['level']
+  t['tier'] = tank['level'].to_i
   t['type'] = tank['tags']
   t['premiumTank'] = premium
   t['turreted'] = has_turret
   t['experienceNeeded'] = 0
-  t['cost'] = tank['price']['value']
-  t['baseHitpoints'] = tank['hull']['maxhealth']
-  t['speedLimit'] = tank['speedlimits']['forward']
-  t['reverseSpeed'] = tank['speedlimits']['backward']
+  t['cost'] = tank['price']['value'].to_i
+  t['baseHitpoints'] = tank['hull']['maxhealth'].to_i
+  t['speedLimit'] = tank['speedlimits']['forward'].to_f
+  t['reverseSpeed'] = tank['speedlimits']['backward'].to_f
   t['camoValue'] = 0
   t['crewLevel'] = 100
   t['stockWeight'] = $weight_hash[tank['userstring']]
   t['hull'] = {}
-  t['hull']['frontArmor'] = [tank['hull']['primaryarmor'][0], 5]
-  t['hull']['sideArmor'] = [tank['hull']['primaryarmor'][1], 5]
-  t['hull']['rearArmor'] = [tank['hull']['primaryarmor'][2], 5]
+  t['hull']['frontArmor'] = [tank['hull']['primaryarmor'][0].to_i, 5]
+  t['hull']['sideArmor'] = [tank['hull']['primaryarmor'][1].to_i, 5]
+  t['hull']['rearArmor'] = [tank['hull']['primaryarmor'][2].to_i, 5]
   if has_turret
     t['turrets'] = {}
     turrets_data = tank['turrets0']
@@ -106,36 +114,141 @@ def tank_hash tank
       t['turrets'][tkey] = {}
       turret = t['turrets'][tkey]
       turret['name'] = tdata['userstring']
-      turret['tier'] = tdata['level']
-      turret['viewRange'] = tdata['circularvisionradius']
-      turret['traverseSpeed'] = tdata['rotationspeed']
-      turret['frontArmor'] = [tdata['primaryarmor'][0], 5]
-      turret['sideArmor'] = [tdata['primaryarmor'][1], 5]
-      turret['rearArmor'] = [tdata['primaryarmor'][2], 5]
-      turret['weight'] = tdata['weight']
+      turret['tier'] = tdata['level'].to_i
+      turret['viewRange'] = tdata['circularvisionradius'].to_i
+      turret['traverseSpeed'] = tdata['rotationspeed'].to_i
+      turret['frontArmor'] = [tdata['primaryarmor'][0].to_i, 5]
+      turret['sideArmor'] = [tdata['primaryarmor'][1].to_i, 5]
+      turret['rearArmor'] = [tdata['primaryarmor'][2].to_i, 5]
+      turret['weight'] = tdata['weight'].to_i
       turret['stockModule'] = false
       turret['topModule'] = false
-      turret['cost'] = tdata['price']
-      turret['additionalHP'] = tdata['maxhealth']
+      turret['cost'] = tdata['price'].to_i
+      turret['additionalHP'] = tdata['maxhealth'].to_i
       turret['availableGuns'] = {}
       guns_data = turrets_data[tkey]['guns']
       guns_data.each do |gkey, gdata|
         t['turrets'][tkey][gkey] = {}
         gun = t['turrets'][tkey][gkey]
         gun['name'] = gdata['userstring']
-        gun['tier'] = gdata['level']
-        gun['rateOfFire'] = (60.0 / gdata['reloadtime'])
-        gun['accuracy'] = gdata['shotdispersionradius']
-        gun['aimTime'] = gdata['aimingtime']
+        gun['tier'] = gdata['level'].to_i
+        gun['rateOfFire'] = (60.0 / gdata['reloadtime'].to_f)
+        gun['accuracy'] = gdata['shotdispersionradius'].to_f
+        gun['aimTime'] = gdata['aimingtime'].to_f
         gun['gunDepression'] = "-#{gdata['pitchlimits'][1]}".to_i
         gun['gunElevation'] = gdata['pitchlimits'][0].to_i.abs
-        gun['weight']
-        gun['stockModule']
-        gun['topModule']
-        gun['cost']
-      end
-    end
-  end
+        gun['weight'] = gdata['weight'].to_i
+        gun['stockModule'] = false
+        gun['topModule'] = false
+        gun['cost'] = gdata['price'].to_i
+        gun['dispersion'] = {}
+        gun['dispersion']['turretRotation'] = 
+          gdata['shotdispersionfactors']['turretrotation'].to_f
+        gun['dispersion']['afterShot'] = 
+          gdata['shotdispersionfactors']['aftershot'].to_f
+        gun['dispersion']['gunDamaged'] = 
+          gdata['shotdispersionfactors']['whilegundamaged'].to_f
+        gun['shells'] = []
+        shells = gdata['shots']
+        shells.each do |skey, sdata|
+          arr = [
+            sdata['piercingpower'][0].to_i,
+            sdata['damage']['armor'].to_i,
+            sdata['price']['value'].to_i,
+            premium_shell?(sdata['price']['kind'])
+          ]
+          gun['shells'].push(arr)
+        end # shell
+      end # gun
+    end # turret
+  else
+    # Deal with turretless vehicles by adding guns to the hull
+    #
+    # "NOT VERY DRY!" You exclaim, and yes this would be much better in different
+    # methods, but for right now I'm just doing this in a purely procedural way
+    # to get it into a workable format, plus dealing with the data as a blob
+    # is proving the simplest way to overcome the manifold annoyances of the
+    # data structuring, mine and theirs. Refactoring will come later. 
+    t['hull']['availableGuns'] = {}
+    guns_data = tank['turrets0']['guns']
+    guns_data.each do |gkey, gdata|
+      t['turrets'][tkey][gkey] = {}
+      gun = t['turrets'][tkey][gkey]
+      gun['name'] = gdata['userstring']
+      gun['tier'] = gdata['level'].to_i
+      gun['rateOfFire'] = (60.0 / gdata['reloadtime'].to_f)
+      gun['accuracy'] = gdata['shotdispersionradius'].to_f
+      gun['aimTime'] = gdata['aimingtime'].to_f
+      gun['gunDepression'] = "-#{gdata['pitchlimits'][1]}".to_i
+      gun['gunElevation'] = gdata['pitchlimits'][0].to_i.abs
+      gun['weight'] = gdata['weight'].to_f
+      gun['stockModule'] = false
+      gun['topModule'] = false
+      gun['cost'] = gdata['price'].to_i
+      gun['dispersion'] = {}
+      gun['dispersion']['turretRotation'] = 
+        gdata['shotdispersionfactors']['turretrotation'].to_f
+      gun['dispersion']['afterShot'] = 
+        gdata['shotdispersionfactors']['aftershot'].to_f
+      gun['dispersion']['gunDamaged'] = 
+        gdata['shotdispersionfactors']['whilegundamaged'].to_f
+      gun['shells'] = []
+      shells = gdata['shots']
+      shells.each do |skey, sdata|
+        arr = [
+          sdata['piercingpower'][0].to_i,
+          sdata['damage']['armor'].to_i,
+          sdata['price']['value'].to_i,
+          premium_shell?(sdata['price']['kind'])
+        ]
+        gun['shells'].push(arr)
+      end #shell
+    end #gun
+  end # has_turret
+  t['engines'] = {}
+  engines_data = tank['engines']
+  engines_data.each do |ekey, edata|
+    t['engines'][ekey] = {}
+    engine = t['engines'][ekey]
+    engine['name'] = edata['userstring']
+    engine['tier'] = edata['level'].to_i
+    engine['horsepower'] = edata['power'].to_i
+    engine['firechance'] = edata['firestartingchance'].to_f
+    engine['weight'] = edata['weight'].to_i
+    engine['stockModule'] = false
+    engine['topModule'] = false
+    engine['cost'] = edata['price'].to_i
+  end # engine
+  t['radios'] = {}
+  radios_data = tank['radios']
+  radios_data.each do |rkey, rdata|
+    t['radios'][rkey] = {}
+    radio = t['radios'][rkey]
+    radio['name'] = rdata['userstring']
+    radio['tier'] = rdata['level'].to_i
+    radio['signalRange'] = rdata['distance'].to_i
+    radio['weight'] = rdata['weight'].to_i
+    radio['stockModule'] = false
+    radio['topModule'] = false
+    radio['cost'] = rdata['price'].to_i
+  end #radio
+  t['suspensions'] = {}
+  suspensions_data = tank['chassis']
+  suspensions_data.each do |ckey, cdata|
+    t['suspensions'][ckey] = {}
+    suspension = t['suspensions'][ckey]
+    suspension['name'] = cdata['userstring']
+    suspension['tier'] = cdata['level'].to_i
+    suspension['loadLimit'] = (cdata['maxload'].to_f / 1000.0)
+    suspension['traverseSpeed'] = cdata['rotationspeed'].to_i
+    suspension['weight'] = cdata['weight'].to_i
+    suspension['pivot'] = cdata['rotationisaroundcenter']
+    suspension['terrainResistance'] = cdata['terrainresistance']
+    suspension['dispersion'] = cdata['shotdispersionfactors']['vehiclerotation'].to_f
+    suspension['stockModule'] = false
+    suspension['topModule'] = false
+    suspension['cost'] = cdata['price'].to_i
+  end # suspension
   return t
 end
 
@@ -150,4 +263,6 @@ def convert_file filename
   final << tank_details(tank)
 end
 
-puts parse_tank("alt_type59.json").to_json
+File.open("t44_converted.json", "w") do |file|
+  file.write(parse_tank("alt_t44.json").to_json)
+end
